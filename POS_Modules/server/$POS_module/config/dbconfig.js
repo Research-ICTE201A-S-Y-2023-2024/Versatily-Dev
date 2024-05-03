408bae9b9a0dc9d03538883c0537d1e743619abc
Koni04 <delgadoargie04@gmail.com>
2024-05-03 22:25:28 +0800

    chore: remove file for new file

--- POS_Modules/server/$POS_module/config/dbconfig.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const database = new Sequelize('posdb', 'root', 'delgado04', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-export default database;
