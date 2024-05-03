3c4cd847804ee2dcc8affbbb22a257550152f3dc
Koni04 <delgadoargie04@gmail.com>
2024-05-03 22:23:08 +0800

    chore: add server configuration, middleware and database connectivity

--- POS_Modules/server/server.js
+++ POS_Modules/server/server.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import database from './config/dbConfig.js';
 import cors from 'cors';
-import sequelize from 'sequelize';
+import WorkbenchRoutes from './routes/workbenchRoutes.js';
 
 const app = express();
 
 const PORT = 5000;
+
+// Database Connection
+try {
+    await database.authenticate();
+    console.log("Database connected successfully")
+} catch (error) {
+    console.log("Connecting Error " + error);
+}
+
+app.use('/workbench', WorkbenchRoutes);
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
