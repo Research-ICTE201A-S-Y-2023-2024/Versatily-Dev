49724ab59fd4039502ac21e5de5dabf134405ac7
Koni04 <delgadoargie04@gmail.com>
2024-05-03 22:06:21 +0800

    chore: import routing by pages

--- POS_Modules/client/src/App.jsx
+++ POS_Modules/client/src/App.jsx
@@ -1,6 +1,9 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import DashboardPage from './Dashboard/Dashboard.jsx'
 import OrderPage from './Order/Order.jsx'
+import Product from './Product/Product.jsx'
+import Table from './Table/Table.jsx'
+import Category from './Category/Category.jsx'
 
 function App() {
 
@@ -10,6 +13,9 @@ function App() {
         <Routes>
           <Route path='/order' element={<OrderPage/>}/>
           <Route path='/dashboard' element={<DashboardPage/>}/>
+          <Route path='/product' element={<Product/>}/>
+          <Route path='/workbench' element={<Table/>}/>
+          <Route path='/categories' element={<Category/>}/>
         </Routes>
       </BrowserRouter>
     </>
