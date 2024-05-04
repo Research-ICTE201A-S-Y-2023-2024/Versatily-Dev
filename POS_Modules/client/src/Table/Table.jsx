3c9fc966d2bd963291282eacd8729a88bbaad84b
Koni04 <delgadoargie04@gmail.com>
2024-05-04 22:31:43 +0800

    chore: fix import, add const, add functons, fix classnames, add main content, remove th email, remove unessesary td

--- POS_Modules/client/src/Table/Table.jsx
+++ POS_Modules/client/src/Table/Table.jsx
@@ -1,10 +1,31 @@
 import './Table.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import profileImage from '../assets/img/profile.jpg';
 import {Link} from 'react-router-dom';
-import '../assets/css/modal.css';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
 const Table = () => {
+    const [workbench, setWorkbench] = useState([]);
+
+    const navigate = useNavigate();
+
+    const handleOrderClick = (id) => {
+        navigate(`/order?workbenchID=${id}`);
+    }
+    
+    const [categories, setCategories] = useState([]);
     const [activeMenuItem, setActiveMenuItem] = useState(0);
+
+    const [isGridView, setIsGridView] = useState(false);
+
+    const toggleListView = () => {
+        setIsGridView(false);
+    };
+
+    const toggleGridView = () => {
+        setIsGridView(true);
+    };
 
     const handleMenuItemClick = (index) => {
         setActiveMenuItem(index); // Set active menu item index
@@ -25,6 +46,29 @@ const Table = () => {
     const closeModal = () => {
         setIsModalOpen(false);
     };
+
+    useEffect(() => {
+        getAllWorkbench();
+        getAllCategory();
+    }, [])
+
+    const getAllWorkbench = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/workbench');
+            setWorkbench(response.data);
+        } catch (error) {
+            console.error("Error fetching the workbenchs", error);
+        }
+    };
+
+    const getAllCategory = async() => {
+        try {
+            const response = await axios.get('http://localhost:5000/categories');
+            setCategories(response.data);
+        } catch (error) {
+            console.error("Error fetching the categories", error);
+        }
+    }
     
     return(
         <>
@@ -149,80 +193,70 @@ const Table = () => {
                         <div className="add-table-container">
                             <button className='add-table' onClick={openModal}>Add Table</button> 
                         </div>
-                        
-                        <div className="workbench-table">
-                            <table>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Email</th>
-                                    <th>Status</th>
-                                    <th>Action</th>
-                                </tr>
-                                <tr>
-                                    <td className="td-id">148352</td>
-                                    <td>khylemyrvin@gmail.com</td>
-                                    <td>Vacant</td>
-                                    <td className='action-icons'>
-                                        <i id="bx-edit" className='bx bx-edit' ></i> 
-                                        <i id="bx-trash" className='bx bx-trash' ></i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="td-id">181645</td>
-                                    <td>user@gmail.com</td>
-                                    <td>Vacant</td>
-                                    <td className='action-icons'>
-                                        <i id="bx-edit" className='bx bx-edit' ></i> 
-                                        <i id="bx-trash" className='bx bx-trash' ></i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="td-id">817466</td>
-                                    <td>user@gmail.com</td>
-                                    <td>Vacant</td>
-                                    <td className='action-icons'>
-                                        <i id="bx-edit" className='bx bx-edit' ></i> 
-                                        <i id="bx-trash" className='bx bx-trash' ></i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="td-id">817656</td>
-                                    <td>user@gmail.com</td>
-                                    <td>Vacant</td>
-                                    <td className='action-icons'>
-                                        <i id="bx-edit" className='bx bx-edit' ></i> 
-                                        <i id="bx-trash" className='bx bx-trash' ></i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="td-id">179165</td>
-                                    <td>user@gmail.com</td>
-                                    <td>Vacant</td>
-                                    <td className='action-icons'>
-                                        <i id="bx-edit" className='bx bx-edit' ></i> 
-                                        <i id="bx-trash" className='bx bx-trash' ></i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="td-id">287253</td>
-                                    <td>user@gmail.com</td>
-                                    <td>Vacant</td>
-                                    <td className='action-icons'>
-                                        <i id="bx-edit" className='bx bx-edit' ></i> 
-                                        <i id="bx-trash" className='bx bx-trash' ></i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="td-id">724655</td>
-                                    <td>user@gmail.com</td>
-                                    <td>Vacant</td>
-                                    <td className='action-icons'>
-                                        <i id="bx-edit" className='bx bx-edit' ></i> 
-                                        <i id="bx-trash" className='bx bx-trash' ></i>
-                                    </td>
-                                </tr>
-                            </table>
+
+                        <div className="app-content-actions-wrapper">
+                            <button className={`action-button list ${!isGridView ? 'active' : ''}`} onClick={toggleListView} title="List View">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-list"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line><line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line></svg>
+                            </button>
+                            <button className={`action-button grid ${isGridView ? 'active' : ''}`} onClick={toggleGridView} title="Grid View">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-grid"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
+                            </button>
                         </div>
+                        <div className={`workbench-container ${isGridView ? 'grid-view' : ''}`}>
+                            {isGridView ? (
+                                <div className="workbench-table grid-view">
+                                    {workbench.length > 0 ? (
+                                        workbench.map((workbenchItem, index) => (
+                                            <div key={index} className="workbench-card">
+                                                <p>Table {workbenchItem.id}</p>
+                                                <p>{workbenchItem.status ? 'Occupied' : 'Vacant'}</p>
+                                                {/* Pinalitan ko from bx bx-cart to AddToCart */}
+                                                <button className='AddToCart' onClick={() => handleOrderClick(workbenchItem.id)}>
+                                                    <i id='add-to-cart' className='bx bx-cart-add'/>
+                                                    Add Order
+                                                </button>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <div className="workbench-card">
+                                            <span>No workbench found.</span>
+                                        </div>
+                                    )}
+                                </div>
+                            ) : (
+                                <div className="workbench-table">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Status</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {workbench.length > 0 ? (
+                                                workbench.map((workbenchItem, index) => (
+                                                    <tr key={index}>
+                                                        <td className="td-id">{workbenchItem.workbenchID}</td>
+                                                        <td>{workbenchItem.status ? "Occupied" : "Vacant"}</td>
+                                                        <td className='action-icons'>
+                                                            <i id="bx-edit" className='bx bx-edit'></i> 
+                                                            <i id="bx-trash" className='bx bx-trash' ></i>
+                                                        </td>
+                                                    </tr>
+                                                ))
+                                            ) : (
+                                                <tr>
+                                                    <td colSpan="3">No workbench found.</td>
+                                                </tr>
+                                            )}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            )}
+                        </div>
+
+
                     </div>
                 </main>
                 {/* MAIN */}
@@ -233,7 +267,7 @@ const Table = () => {
             {isModalOpen && (
                 <div className="modal">
                     <div className="overlay"></div>
-                        <div className="modal-content">
+                        <div className="modal-workbench">
                             <h2 className="add-table-t">Add Table</h2>
                             <hr></hr>
                             <form className='form'>
@@ -250,9 +284,9 @@ const Table = () => {
                                     <label>Vacant</label>
                                 </div>
                                 
-                                <div className='btn-container'>
-                                    <button type='button' className='btn-cancel' onClick={closeModal}>Cancel</button>
-                                    <button type='submit' className='btn-submit'>Submit</button>
+                                <div className='workbench-btn-container'>
+                                    <button type='button' className='workbench-btn-cancel' onClick={closeModal}>Cancel</button>
+                                    <button type='submit' className='workbench-btn-submit'>Submit</button>
                                 </div>
                         </form>
                     </div>
