316a34caf7184326ee02649f0159da43ff62d9be
Koni04 <delgadoargie04@gmail.com>
2024-05-04 23:12:30 +0800

    chore: add import, add router method

--- POS_Modules/server/routes/categoryRoutes.js
+++ POS_Modules/server/routes/categoryRoutes.js
@@ -1,6 +1,7 @@
 import {
     getAllCategory,
-    saveCategory
+    saveCategory,
+    getCategoryById
 } from '../controllers/CategoryController.js';
 import express from 'express';
 
@@ -8,5 +9,6 @@ const router = express.Router();
 
 router.get('/', getAllCategory);
 router.post('/', saveCategory);
+router.get('/:id', getCategoryById);
 
 export default router;
