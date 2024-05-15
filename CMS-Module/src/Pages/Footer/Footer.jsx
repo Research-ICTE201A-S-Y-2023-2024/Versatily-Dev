22a207a3f67321b8fcd6536beca26d9259b4c93c
Ashen360 <115592969+Ashen360@users.noreply.github.com>
2024-05-15 20:01:29 +0800

    Initial Commit of CMS-Module

---
+++ CMS-Module/src/Pages/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+// src/Pages/Footer/Footer.js
+import React from 'react';
+import './FooterStyles.css';
+import logo from '../../assets/Logo-Beige.png';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-logo">
+          <img src={logo} alt="Logo" />
+          <span className="name">Versatily</span>
+        </div>
+      <div className="footer-container">
+        <div className="footer-section">
+          <h4>About</h4>
+          <ul>
+            <li><a href="#">About Page</a></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>Support</h4>
+          <ul>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Support Page 2</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
