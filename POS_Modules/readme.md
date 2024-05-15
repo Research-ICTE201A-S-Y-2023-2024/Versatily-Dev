dcb804c97803d31c0ef818daad5d702370fe10cb
Argie P. Delgado <151521279+Koni04@users.noreply.github.com>
2024-05-16 06:32:30 +0800

    Delete POS_Modules directory

--- POS_Modules/readme.md
+++
@@ -1,9 +0,0 @@
-# Versatily-Develop System
-
-- Point of Sales Project'
-
-First you should clone the folder of the POS_Modules and
-then you just need to type "npm install" on terminal.
-
-### Description
-A standalone POS System for upcoming G12 exposition event in STI College Marikina
--- POS_Modules/server/routes/transactionRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {getAllTransaction, getTableTransactionsItems} from '../controllers/transactionController.js';
-
-const router = express.Router();
-
-router.get('/', getAllTransaction);
-router.get('/:transactionId', getTableTransactionsItems);
-
-export default router;
