a6a885f29817e1fd30645d5f98d4d4f060ce59ce
VanzTyl <125661587+VanzTyl@users.noreply.github.com>
2024-05-16 19:12:11 +0800

    Delete UMS directory

--- UMS/server/UMS_Modules/config/dbconfig.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize'
-
-// MySQL connection pool
-const database = new Sequelize('VersatilyDB', 'root', '1234', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-export default database;
